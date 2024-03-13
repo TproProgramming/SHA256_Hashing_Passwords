30436c93458ced41f1360eb0bcdcaeb87ac261c2
Tyler <tyler@provenchermultimedia.com>
2024-03-13 17:51:44 -0400

    Added Salting
    Salting provides additional security. The password output will never be the same

--- hashing_passwords.py
+++ hashing_passwords.py
@@ -1,10 +1,29 @@
 import hashlib
+import secrets
 
-def hash_password(password):
-    #Hash the password using Secure Hasing Algorithm 256-bit
-    hashed_password = hashlib.sha256(password.encode()).hexdigest()
-    return hashed_password
+
+def salt_password(password):
+    #Generate a random salt
+    salt = password + secrets.token_hex(16)
+    return salt
     
-password = input("Enter your password to be hashed: ")
-hashed_password = hash_password(password)
-print(f"Hashed Password: {hashed_password}") 
+def hash_password(salted_password):
+    #Hash the password using Secure Hasing Algorithm 256-bit
+    hashed_password = hashlib.sha256(salted_password.encode()).hexdigest()
+    return hashed_password
+
+def main():   
+    password = input("Enter your password to be hashed: ")
+
+    #Salt password
+    salted_password = salt_password(password)
+
+    #Create hashed password
+    hashed_password = hash_password(salted_password)
+
+    print(f"Plain Text Password: {password}")
+    print(f"Salted Password {salted_password}") 
+    print(f"Hashed Password: {hashed_password}")
+
+if __name__ == "__main__":
+    main()
